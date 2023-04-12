48d77b47acf45e12ec365d0bdde5860c8f4033c3
Jacky <jackypengsmail@gmail.com>
2023-04-11 21:38:47 -0400

    Stripe demo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
+  async function checkout() {
+    console.log("asdasd");
+    try {
+      const response = await axios({
+        method: "post",
+        url: "http://localhost:4000/payment",
+        headers: { "Content-Type": "application/json" },
+        //data will equal to payment fee of service id instead of items
+        data: {
+          // id: 1
+          items: [
+            { id: 1, quantity: 3 },
+            { id: 2, quantity: 1 },
+          ],
+        },
+      });
+      console.log(response)
+      if (response) {
+        window.location = response.data.url
+      }
+
+    } catch (e) {
+      console.log(e);
+    }
+    // () => {
+    //   fetch("http://localhost:3001/payment", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({
+    //       items: [
+    //         { id: 1, quantity: 3 },
+    //         { id: 2, quantity: 1 },
+    //       ],
+    //     }),
+    //   })
+    //     .then((res) => {
+    //       console.log(res)
+    //       if (res.ok) return res.json();
+    //       return res.json().then((json) => Promise.reject(json));
+    //     })
+    //     .then(({ url }) => {
+    //       window.location = url;
+    //     })
+    //     .catch((e) => {
+    //       console.error(e.error);
+    //     });
+    // };
+  }
   return (
     <div className="App">
       <div>
@@ -28,8 +79,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <button onClick={checkout}>Checkout</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
