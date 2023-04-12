afc63ab8e0a50c5412ab31015c80fc049ff609db
Jacky <jackypengsmail@gmail.com>
2023-04-11 21:38:13 -0400

    Implemented stripe payment foundation

---
+++ backend/routes/payment.js
@@ -0,0 +1,65 @@
+import express from "express";
+import Stripe from "stripe";
+
+const router = express.Router();
+
+const stripe = new Stripe(process.env.STRIPE_KEY);
+const storeItems = new Map([
+  [1, { priceInCents: 10000, name: "100 Dollars" }],
+  [2, { priceInCents: 200000, name: "Two thousand" }],
+]);
+
+router.post("/", async (request, response) => {
+    console.log(request.body.items);
+  //request.body.payment_fee instead of storeItems
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: request.body.items.map((item) => {
+        const storeItem = storeItems.get(item.id);
+        return {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: storeItem.name,
+            },
+            unit_amount: storeItem.priceInCents,
+          },
+          quantity: item.quantity,
+        };
+      }),
+      success_url: "http://localhost:4000/success.html",
+      cancel_url: "http://localhost:4000/",
+    });
+    console.log(session.line_items);
+    // const session = await stripe.checkout.sessions.create({
+    //   payment_method_types: ["card"],
+    //   mode: "payment",
+    //   line_items: [
+    //     {
+    //       price_data: {
+    //         currency: "usd",
+    //         product_data: {
+    //           name: "Plumbing",
+    //         },
+    //         unit_amount: 70000,
+    //         quantity: 1,
+    //       },
+    //     },
+    //   ],
+
+    //   success_url: "http://localhost:4000/success.html",
+    //   cancel_url: "http://localhost:4000/",
+    // });
+    // console.log(session);
+    response.json({
+      url: session.url,
+    });
+  } catch (e) {
+    console.log(e);
+    response.status(500).json({ error: e.message });
+  }
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import authRouter from "./routes/auth.js";
-import serviceRoute from "./routes/service.js";
+import serviceRouter from "./routes/service.js";
+import paymentRouter from "./routes/payment.js";
+import Stripe from "stripe";
+import cors from "cors";
 
 dotenv.config();
 export default function createServer() {
   const app = express();
+
+  app.use(cors());
 
   app.use(express.json());
 
@@ -14,7 +19,9 @@ export default function createServer() {
 
   app.use("/auth", authRouter);
 
-  app.use("/service", serviceRoute);
+  app.use("/service", serviceRouter);
+
+  app.use("/payment", paymentRouter);
 
   return app;
 }
