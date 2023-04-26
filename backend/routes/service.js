45ce21d22597f1d6df3c39bee15ee7e80c2d22a3
Jacky <jackypengsmail@gmail.com>
2023-04-25 22:27:28 -0400

    Connect signup and login, API calls to auth routes

--- backend/routes/service.js
+++ backend/routes/service.js
@@ -1,172 +1,171 @@
 import express from "express";
 import prisma from "../db/index.js";
 
+export default function serviceRouter(passport) {
+  const router = express.Router();
 
-const router = express.Router();
-
-//GET all services 
-router.get("/", async (_request, response) => {
+  //GET all services
+  router.get("/", async (_request, response) => {
     try {
-        const allService = await prisma.service.findMany();
+      const allService = await prisma.service.findMany();
 
-        if (allService) {
-            response.status(200).json({
-                success: true,
-                services: allService
-            });
-        }
-        else {
-            response.status(404).json({
-                success: false,
-                message: "services does not exist"
-            })
-        }
-
-
+      if (allService) {
+        response.status(200).json({
+          success: true,
+          services: allService,
+        });
+      } else {
+        response.status(404).json({
+          success: false,
+          message: "services does not exist",
+        });
+      }
     } catch (error) {
-        response.status(500).json({
-            success: false,
-            message: "Something went wrong"
-        });
+      response.status(500).json({
+        success: false,
+        message: "Something went wrong",
+      });
     }
+  });
 
-});
-
-
-//GET all bookings based on service type
-router.get("/booking", async (request, response) => {
+  //GET all bookings based on service type
+  router.get("/booking", async (request, response) => {
     try {
-        const services = await prisma.service.findMany({
-            where: {
-                service_type: request.body.service_type,
-            },
-            include: {
-                booking: true
-            }
-        });
+      const services = await prisma.service.findMany({
+        where: {
+          service_type: request.body.service_type,
+        },
+        include: {
+          booking: true,
+        },
+      });
 
-        if (services) {
-            response.status(200).json({
-                success: true,
-                services,
-            })
-        }
+      if (services) {
+        response.status(200).json({
+          success: true,
+          services,
+        });
+      }
     } catch (error) {
-        response.status(500).json({
-            success: false,
-            message: "Something went wrong"
-        });
+      response.status(500).json({
+        success: false,
+        message: "Something went wrong",
+      });
     }
-});
+  });
 
-
-
-
-//POST new service
-router.post("/", async (request, response) => {
-    try {
+  //POST new service
+  router.post(
+    "/",
+    passport.authenticate("jwt", { session: false }),
+    async (request, response) => {
+      console.log(request.body);
+      try {
         const newService = await prisma.service.create({
-            data: {
-                service_type: request.body.service_type,
-                payment_id: request.body.payment_id,
-                booking_id: request.body.booking_id,
-                provider_id: request.body.provider_id,
-                booking_date: request.body.booking_date,
-            }
+          data: {
+            service_type: request.body.service_type,
+            payment_id: request.body.payment_id,
+            booking_id: request.body.booking_id,
+            provider_id: request.body.provider_id,
+            booking_date: request.body.booking_date,
+            payment_fee: request.body.payment,
+          },
         });
 
         if (newService) {
-            response.status(201).json({
-                success: true,
-                message: "service has been created",
-            })
+          response.status(201).json({
+            success: true,
+            message: "service has been created",
+          });
         } else {
-            response.status(500).json({
-                success: false,
-                message: "failed to create service",
-            })
-        }
-    } catch (error) {
-        response.status(500).json({
+          response.status(500).json({
             success: false,
-            message: "Something went wrong"
+            message: "failed to create service",
+          });
+        }
+      } catch (error) {
+        console.log(error);
+        response.status(500).json({
+          success: false,
+          message: "Something went wrong",
         });
+      }
     }
-});
+  );
 
-
-//PUT existing service by id
-router.put("/:serviceId", async (request, response) => {
-    try {
+  //PUT existing service by id
+  router.put(
+    "/:serviceId",
+    passport.authenticate("jwt", { session: false }),
+    async (request, response) => {
+      try {
         const serviceId = parseInt(request.params.serviceId);
 
         const findByServiceId = await prisma.service.update({
-            where: {
-                service_id: serviceId
-            },
-            data: {
-                service_type: request.body.service_type,
-                payment_id: request.body.payment_id,
-                booking_id: request.body.booking_id,
-                provider_id: request.body.provider_id,
-                booking_date: request.body.booking_date,
-            }
+          where: {
+            service_id: serviceId,
+          },
+          data: {
+            service_type: request.body.service_type,
+            payment_id: request.body.payment_id,
+            booking_id: request.body.booking_id,
+            provider_id: request.body.provider_id,
+            booking_date: request.body.booking_date,
+          },
         });
 
         if (findByServiceId) {
-            response.status(201).json({
-                success: true,
-                message: "service has been updated",
-            });
+          response.status(201).json({
+            success: true,
+            message: "service has been updated",
+          });
         } else {
-            response.status(500).json({
-                success: false,
-                message: "failed to update service",
-            });
-        }
-    } catch (error) {
-        response.status(500).json({
+          response.status(500).json({
             success: false,
-            message: "Something went wrong"
+            message: "failed to update service",
+          });
+        }
+      } catch (error) {
+        response.status(500).json({
+          success: false,
+          message: "Something went wrong",
         });
+      }
     }
-});
+  );
 
-
-//DELETE service by id
-router.delete("/:serviceId", async (request, response) => {
-    try {
+  //DELETE service by id
+  router.delete(
+    "/:serviceId",
+    passport.authenticate("jwt", { session: false }),
+    async (request, response) => {
+      try {
         const serviceId = parseInt(request.params.serviceId);
 
         const findByServiceId = await prisma.service.delete({
-            where: {
-                service_id: serviceId,
-            }
+          where: {
+            service_id: serviceId,
+          },
         });
 
         if (findByServiceId) {
-            response.status(201).json({
-                success: true,
-                message: "service has been deleted",
-            });
+          response.status(201).json({
+            success: true,
+            message: "service has been deleted",
+          });
         } else {
-            response.status(500).json({
-                success: false,
-                message: "failed to delete service",
-            });
-        }
-    } catch (error) {
-        response.status(500).json({
+          response.status(500).json({
             success: false,
-            message: "Something went wrong"
+            message: "failed to delete service",
+          });
+        }
+      } catch (error) {
+        response.status(500).json({
+          success: false,
+          message: "Something went wrong",
         });
+      }
     }
-})
-
-
-export default router;
-
-
-
-
-
+  );
+  return router;
+}
--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,24 @@ import express from "express";
 import * as dotenv from "dotenv";
 import authRouter from "./routes/auth.js";
 
+// import serviceRouter from "./routes/service.js";
 import serviceRouter from "./routes/service.js";
 import paymentRouter from "./routes/payment.js";
 import Stripe from "stripe";
 import cors from "cors";
+import passport from "passport";
+import setupJWTStrategy from "./auth/index.js";
 
 import bookingRoute from "./routes/booking.js";
-
+import providerRouter from "./routes/provider.js";
+import clientRouter from "./routes/client.js";
+// import bookingRoute from "./routes/booking.js"
 
 dotenv.config();
 export default function createServer() {
   const app = express();
 
-
-  app.use(cors())
+  app.use(cors());
 
   app.use(express.json());
 
@@ -24,17 +28,18 @@ export default function createServer() {
 
   app.use("/auth", authRouter);
 
-  app.use("/service", serviceRouter);
+  app.use(
+    "/service",
+    serviceRouter(passport)
+  );
 
   app.use("/payment", paymentRouter);
-
 
   app.use("/provider", providerRouter(passport));
 
   app.use("/client", clientRouter(passport));
 
-  app.use("/booking", bookingRoute());
-
+  app.use("/booking", bookingRoute(passport));
 
   return app;
 }
