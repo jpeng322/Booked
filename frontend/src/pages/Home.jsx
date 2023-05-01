b840002dcf40971c3dc9431538e2e80eb402017b
Hiwot Woldesemayat <hiwot.woldesemayat@fdmgroup.com>
2023-04-30 20:38:52 -0400

    Subscription email component - Home and Featured providers - Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import SubscriptionBox from './SubscriptionBox';
 
 const Home = () => {
+
   async function checkout() {
     console.log("asdasd");
     try {
@@ -63,6 +66,31 @@ const Home = () => {
         <button onClick={() => navigate("/signup")}>Signup</button>
         <button onClick={() => navigate("/login")}>Login</button>
       </div>
+      <div>
+        <Container style={{ marginTop: "473px" }}>
+          <Row>
+            <Col md={4}>
+              <Image src="https://via.placeholder.com/367x225" thumbnail />
+            </Col>
+            <Col md={4}>
+              <Image src="https://via.placeholder.com/367x225" thumbnail />
+            </Col>
+            <Col md={4}>
+              <Image src="https://via.placeholder.com/367x225" thumbnail />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div>
+        <Container className="d-flex justify-content-center" style={{ padding: "40px" }}> 
+          <h1>Wanna be in the know about the Pros? Subscribe</h1>
+        </Container> 
+        <Container className="d-flex justify-content-center" style={{ padding: "10px" }}> 
+        <h6 style={{ fontWeight: 'bold' }}>Join  Our Email list by adding your email below</h6>
+        </Container> 
+       
+        <SubscriptionBox />
+    </div>
     </div>
   );
 };
