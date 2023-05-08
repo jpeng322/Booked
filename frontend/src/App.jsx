74f262387519db8792bcd5bba0919192c7b15738
Jacky <jackypengsmail@gmail.com>
2023-05-08 18:37:20 -0400

    Prettied App.jsx structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
-
-import About from "../src/pages/About"
+import About from "../src/pages/About";
 import "/src/App.css";
 import ProviderCard from "./pages/ProviderCard";
 import providers from "./providers";
@@ -22,7 +21,6 @@ import CustomerAcc from "./pages/CustomerBookings";
 import FavoriteProviders from "./components/Favoritess";
 import { fetchLogin, fetchSignup } from "./api";
 import CustomerAccountContact from "./pages/CustomerAccountContact";
-
 
 function App() {
   const [count, setCount] = useState(0);
@@ -51,122 +49,124 @@ function App() {
       console.log(e);
     }
   }
-    // () => {
-    //   fetch("http://localhost:3001/payment", {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify({
-    //       items: [
-    //         { id: 1, quantity: 3 },
-    //         { id: 2, quantity: 1 },
-    //       ],
-    //     }),
-    //   })
-    //     .then((res) => {
-    //       console.log(res)
-    //       if (res.ok) return res.json();
-    //       return res.json().then((json) => Promise.reject(json));
-    //     })
-    //     .then(({ url }) => {
-    //       window.location = url;
-    //     })
-    //     .catch((e) => {
-    //       console.error(e.error);
-    //     });
-    // };
+  // () => {
+  //   fetch("http://localhost:3001/payment", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       items: [
+  //         { id: 1, quantity: 3 },
+  //         { id: 2, quantity: 1 },
+  //       ],
+  //     }),
+  //   })
+  //     .then((res) => {
+  //       console.log(res)
+  //       if (res.ok) return res.json();
+  //       return res.json().then((json) => Promise.reject(json));
+  //     })
+  //     .then(({ url }) => {
+  //       window.location = url;
+  //     })
+  //     .catch((e) => {
+  //       console.error(e.error);
+  //     });
+  // };
   // }
-    const router = createBrowserRouter([
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "auth/loginClient",
-        element: <LoginClient />,
-        action: async ({ request }) => {
-          try {
-            const formData = await request.formData();
-            const email = formData.get("email");
-            const password = formData.get("password");
-            return await fetchLogin(email, password);
-
-          } catch (error) {
-            return error;
-          }
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "auth/loginClient",
+      element: <LoginClient />,
+      action: async ({ request }) => {
+        try {
+          const formData = await request.formData();
+          const email = formData.get("email");
+          const password = formData.get("password");
+          return await fetchLogin(email, password);
+        } catch (error) {
+          return error;
         }
       },
-      {
-        path: "auth/signup",
-        element: <Signup />,
-        action: async ({ request }) => {
-          try {
-            const formData = Object.fromEntries(await request.formData());
-            const { email, password, firstName, lastName, phoneNumber } = formData
-            // console.log(email, password, firstName, lastName, phoneNumber);
-            return await fetchSignup(email, password, firstName, lastName, phoneNumber);
-          } catch (error) {
-            return error;
-          }
+    },
+    {
+      path: "auth/signup",
+      element: <Signup />,
+      action: async ({ request }) => {
+        try {
+          const formData = Object.fromEntries(await request.formData());
+          const { email, password, firstName, lastName, phoneNumber } =
+            formData;
+          // console.log(email, password, firstName, lastName, phoneNumber);
+          return await fetchSignup(
+            email,
+            password,
+            firstName,
+            lastName,
+            phoneNumber
+          );
+        } catch (error) {
+          return error;
         }
       },
-      {
-        path: "auth/login",
-        element: <Login />,
-        action: async ({ request }) => {
-          try {
-            const formData = await request.formData();
-            const email = formData.get("email");
-            const password = formData.get("password");
-            return await fetchLogin(email, password);
-
-          } catch (error) {
-            return error;
-          }
-          
+    },
+    {
+      path: "auth/login",
+      element: <Login />,
+      action: async ({ request }) => {
+        try {
+          const formData = await request.formData();
+          const email = formData.get("email");
+          const password = formData.get("password");
+          return await fetchLogin(email, password);
+        } catch (error) {
+          return error;
         }
       },
-      {
-        path: "/customeraccount",
-        element: <CustomerAccountContact />,
-        
-      },
-      {
-        path: "/preferences",
-        element: <OnboardingSurvey />,
-      },
-      {
-        path: "/recommendations",
-        element: <Recommendations />,
-      },
-      {
-        path: "/profile",
-        element: <Profile />,
-      },
-      {
-        path: "/customerbookings",
-        element: <CustomerAcc />,
-      },
-      {
-        path: "/carousel",
-      element: <FavoriteProviders />
-    }, {
-    path: "/provider",
-    element: <ProviderCard providers={providers} />},
+    },
+    {
+      path: "/customeraccount",
+      element: <CustomerAccountContact />,
+    },
+    {
+      path: "/preferences",
+      element: <OnboardingSurvey />,
+    },
+    {
+      path: "/recommendations",
+      element: <Recommendations />,
+    },
+    {
+      path: "/profile",
+      element: <Profile />,
+    },
+    {
+      path: "/customerbookings",
+      element: <CustomerAcc />,
+    },
+    {
+      path: "/carousel",
+      element: <FavoriteProviders />,
+    },
+    {
+      path: "/provider",
+      element: <ProviderCard providers={providers} />,
+    },
     {
       path: "/about",
-      element: <About />},
-    
-    ]);
+      element: <About />,
+    },
+  ]);
   return (
     <div className="App">
-          <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </div>
   );
-  }
-
-
-
+}
 
 export default App;
