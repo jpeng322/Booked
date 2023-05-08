77283bb935550c01f16344b7608e9f577dfd7b41
Jacky <jackypengsmail@gmail.com>
2023-05-08 18:35:31 -0400

    Update customer booking page, made booking display dynamic, added scheduling to provider profile

--- frontend/src/pages/CustomerBookings.jsx
+++ frontend/src/pages/CustomerBookings.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Container, Col, Row } from "react-bootstrap";
 import BookingsCard from "../components/BookingsCard";
 import FavoriteProviders from "../components/FavoritesComp";
-import BookingsTabs from "../components/BookingsTabs";
+import BookingsTabs from "../components/BookingTab/BookingsTabs";
 
 //css
 import "../CSS/CustomerBookings.css";
 const CustomerBookings = () => {
   return (
-    <Container fluid className="customerbookings-container pe-md-4 ps-md-4 pe-lg-5 ps-lg-5 m-0">
+    <Container
+      fluid
+      className="customerbookings-container pe-md-4 ps-md-4 pe-lg-5 ps-lg-5 m-0"
+    >
       <Row className="m-0">
         <h1>Manage Bookings</h1>
         <BookingsTabs />
