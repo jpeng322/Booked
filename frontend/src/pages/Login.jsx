942bcc65b57396a2a94a5457f21aee5feb82ce81
kem02 <molinakevin80@gmail.com>
2023-05-29 01:15:40 -0400

    added hifi to login pages and contact

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -8,6 +8,9 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Stack from 'react-bootstrap/Stack';
+import Image from 'react-bootstrap/Image';
+import bgImg from '../images/login-background.png'
+import logo from '../images/logo.png'
 
 
 
@@ -34,10 +37,34 @@ const Login = () => {
   }
 
 
-  // console.log(actionData);
 
+  const style = {
+    label: {
+      color: '#B1660E',
+      fontSize: '24px',
+      // border: '1px solid black',
+      marginBottom: '0',
+      fontWeight: '550',
+      marginLeft: '6px'
+    },
+    control: {
+      border: '1px solid #B1660E',
+      borderRadius: '10px'
 
+    },
+    hypertext: {
+      color: '#476685',
+      fontSize: '12px',
+      textDecoration: 'none'
+    },
+    hypertextBottom: {
+      color: '#476685',
+      fontSize: '16px',
+      textDecoration: 'none',
+      fontWeight: '500'
+    },
 
+  }
 
 
 
@@ -47,43 +74,80 @@ const Login = () => {
     <div
       style={{
         minHeight: "100vh",
-        border: "1px solid black",
+        // border: "1px solid black",
+        backgroundImage: `url(${bgImg})`,
+        backgroundRepeat: "no-repeat",
+        backgroundAttachment: 'fixed',
+        backgroundPosition: "center center",
+        backgroundSize: "cover",
+        overflow: 'auto'
       }}
     >
       <Stack
         className="col-lg-3 col-md-6 col-sm-10  mx-auto fw-bold rounded-2"
-        // className="d-flex align-items-center justify-content-center fw-bold"
         style={{
-          backgroundColor: 'lightgrey',
-          padding: '4rem',
-          border: '1px solid black',
-          marginTop: '250px',
-          // marginRight: '500px',
-          // marginLeft: '500px',
-          // minHeight: '100vh'
+          backgroundColor: '#F9EDB4',
+          padding: '2rem 4rem 3rem 4rem',
+          border: '2px solid #B1660E',
+          marginTop: '120px',
+
         }}
       >
         <Container
           style={{
             // border: '1px solid black',
-            paddingTop: '4rem'
+            // paddingTop: '4rem'
           }}
         >
+
+
+          <Col
+            className='d-flex justify-content-center'
+            style={{
+              // border: '1px solid #B1660E',
+              padding: '0px',
+              height: '100%'
+            }}
+          >
+            <Image
+              src={logo}
+              style={{
+                // border: '1px solid blue',
+                width: '12rem',
+                height: '10rem',
+              }}
+            />
+          </Col>
+
+          <h1
+            className="text-center fw-normal"
+            style={{
+              // border: '1px solid blue',
+              color: '#B1660E',
+              fontSize: '38px'
+            }}
+          >
+            Provider Login
+          </h1>
+
+
+
+
           <Row>
             <Col
             // lg={{ span: 4, offset: 4 }}
             // md={{ span: 0, offset: 0 }}
             >
-              <Form onSubmit={handleSubmit(onSubmit)}>
-                <h1 className="text-center">Provider Login</h1>
+              <Form className='mb-2' onSubmit={handleSubmit(onSubmit)}>
+
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label>Username</Form.Label>
-                  <Form.Control type="email" placeholder="Email" {...register("email", { required: true, maxLength: 80 })} />
+                  <Form.Label style={style.label}>Username</Form.Label>
+                  <Form.Control style={style.control} type="email" placeholder="Email" {...register("email", { required: true, maxLength: 80 })} />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="text" placeholder="Password" {...register("password", { required: true, maxLength: 100 })} />
+                  <Form.Label style={style.label}>Password</Form.Label>
+                  <Form.Control style={style.control} type="text" placeholder="Password" {...register("password", { required: true, maxLength: 100 })} />
                 </Form.Group>
 
                 <Stack
@@ -98,11 +162,11 @@ const Login = () => {
                     // border: '1px solid black',
                   }}
                 >
-                  <a href="#" className="text-primary" onClick={() => { }}>
+                  <a href="#" style={style.hypertext} onClick={() => { }}>
                     Forgot username?
                   </a>
                   <div class="vr"></div>
-                  <a href="#" className="text-primary" onClick={() => { }}>
+                  <a href="#" style={style.hypertext} onClick={() => { }}>
                     Forgot password?
                   </a>
                 </Stack>
@@ -114,29 +178,38 @@ const Login = () => {
                     paddingBottom: '2rem',
                   }}
                 >
-                  <a href="#" className="text-primary" onClick={() => navigate("/auth/signup") }>
-                    Create an account
+                  <a href="#" style={style.hypertextBottom} onClick={() => navigate("/auth/signup")}>
+                    join our network
                   </a>
                 </div>
 
-                <Stack className="col-lg-10 col-md-10 col-sm-10 mx-5">
+                <Stack className="col-lg-12 col-md-12 col-sm-12">
                   <Button
-                    variant="primary"
                     type="submit"
                     className='rounded-pill'
                     style={{
-                      padding: '12px',
-                      fontWeight: 'bold',
-                      fontSize: '20px'
+                      padding: '8px',
+                      margin: '20px 10px 20px 10px',
+                      fontWeight: '400',
+                      fontSize: '25px',
+                      color: '#476685',
+                      backgroundColor: '#F1A855',
+                      border: '2px solid #B1660E'
                     }}
                   >
                     Login
                   </Button>
                 </Stack>
-
-
-
               </Form>
+
+              <Col
+                className='d-flex justify-content-center '
+              >
+                <a href='#' style={style.hypertextBottom}>Login as Customer</a>
+              </Col>
+
+
+
             </Col>
           </Row>
         </Container>
@@ -150,13 +223,7 @@ const Login = () => {
 
 
 
-      {/* <div>LOGIN</div>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="email" placeholder="Email" {...register("email", { required: true, maxLength: 80 })} />
-        <input type="text" placeholder="Password" {...register("password", { required: true, maxLength: 100 })} />
 
-        <input type="submit" />
-      </form> */}
     </div>
 
   )
